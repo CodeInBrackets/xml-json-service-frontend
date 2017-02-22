1fb581c91757b4857ce5b4c62780d15e3c273e5f
alvaro <Alvarojosepl@gmail.com>
2017-02-22 15:15:26 -0600

    add skeleton for application

---
+++ server/server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var path = require('path')
+var app = express();
+
+var isDevMode = process.argv.includes('--dev');
+var port = process.env.PORT;
+
+if(!port)
+    port=3000
+
+
+if(isDevMode)
+    require('./webpack-load')(app);
+
+app.get('/', function (req, res) {
+    res.sendFile(path.resolve(__dirname + '/../client/views/index.html'));
+});
+
+app.listen(port);
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var entryPoints = ['./client/index.js']
+
+if (process.env.NODE_ENV !== 'production')
+  entryPoints.push('webpack-hot-middleware/client?reload=true')
+
+module.exports = {
+  context: path.join(__dirname),
+  entry: entryPoints,
+  output: {
+    path: path.join(__dirname, 'www'),
+    filename: 'bundle.js',
+    publicPath: '/www'
+  },
+  resolve: {
+      modules: [
+          path.join(__dirname, "client"),
+            "node_modules"
+    ]
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query:{
+            presets: ["latest"],
+            comments: false
+        }
+      }
+    ]
+  },
+  plugins: [
+      new webpack.optimize.OccurrenceOrderPlugin(),
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoEmitOnErrorsPlugin()
+  ],
+  devtool: '#eval-source-map'
+};
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin()
+  ])
+}
