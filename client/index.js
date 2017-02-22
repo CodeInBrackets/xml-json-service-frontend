4e67aa0c3a62dac37e5e04cfd07fb936507530c1
alvaro <Alvarojosepl@gmail.com>
2017-02-22 16:23:07 -0600

    added partial view with css pattern

--- client/index.js
+++ client/index.js
@@ -2,14 +2,19 @@ function initialize(){
     var Aggregator = require('./event-aggregator.js');
     var APIHandler = require('./api-handler.js');
 
+    var mainView = require('./views/partials/main.js');
+
     var apiRequestModel= require('./models/api-request.js');
     var apiRequestPresenter = require('./presenters/api-request.js');
     var apiRequestView = require('./views/partials/api-request.js');
 
-    var IndexStyle = require('./views/styles/index.styl');
+    var IndexStyle = require('./views/styles/main.styl');
     var apiRequestStyle = require('./views/styles/api-request.styl');
 
     document.getElementById('app')
+        .insertAdjacentHTML('beforeend', mainView);
+
+    document.getElementById('content')
         .insertAdjacentHTML('beforeend', apiRequestView);
 }
 
---
+++ client/views/partials/main.js
@@ -0,0 +1,6 @@
+var html = '<div class ="wrap">\
+              <div id="header"></div> \
+              <div id="content"></div> \
+            </div>'
+
+module.exports = html
