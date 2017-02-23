6c10649d9e1069c6565630bb04d83d6582bd6394
alvaro <Alvarojosepl@gmail.com>
2017-02-23 16:35:27 -0600

    fix html

--- client/views/partials/api-request.js
+++ client/views/partials/api-request.js
@@ -1,11 +1,11 @@
 var html = '<div id="_name_">                                                                   \
                 <div id="top">                                                                  \
-                    <h1 id="command_name"><h1/>                                                 \
+                    <h1 id="command_name"></h1>                                                 \
                     <div id="command_dropdown" class="dropdown">                                \
                         <button id="command_dropdown_button" class="dropbtn">Dropdown</button>  \
-                        <div id="command_dropdown_content" class="dropdown-content">            \
-                        </div>                                                                  \
+                        <div id="command_dropdown_content" class="dropdown-content"></div>      \
                     </div>                                                                      \
+                </div>                                                                          \
                 <div id="request"></div>                                                        \
                 <div id="response"></div>                                                       \
             </div> '
