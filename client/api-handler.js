4607c0177c1d049ee2fd5bc936f986934bdea615
alvaro <Alvarojosepl@gmail.com>
2017-02-23 03:40:45 -0600

    added mocked call

--- client/api-handler.js
+++ client/api-handler.js
@@ -1,11 +1,17 @@
 class apiHandler{
     constructor(aggregator){
         this._aggregator = aggregator;
-        aggregator.on("api:search:request:json", (request, idExtra) => {
-
-        });
-        aggregator.on("api:search:request:xml", (request, idExtra) => {
-
+        this._aggregator.on("api:search:request:advertiser", (request) => {
+                this._aggregator.trigger("api:search:results:advertiser", {
+                    "id": 1,
+                    "name": "MockAd",
+                    "external_id": "1",
+                    "impressions": 53221221,
+                    "clicks": 45121
+                });
+            // $.post('/search', request.Json, (data) => {
+            //     this._aggregator.trigger("api:search:results:advertiser", data, idExtra);
+            // },"json");
         });
     }
 }
